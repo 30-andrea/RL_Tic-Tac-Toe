5b7f51c506428bdc4adc115318707f351c1afc44
rjaditya-2702 <jannali.a@northeastern.edu>
2024-06-01 14:37:45 -0400

    Updating the template for Multi agent system. Reset: empty board Step_p1: gets the action of player 1 and updates the board and reward for the move Step_p2: same function but for player 2 (culmulative reward is obtained during training.) One player has +ve reward for win& draw while the other has +ve reward for win only

--- env/board.py
+++ env/board.py
@@ -1,24 +1,25 @@
 import numpy as np
 import pygame
-
 import gymnasium as gym
 from gymnasium import spaces
+from tabulate import tabulate
 
 class TicTacToe(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 16}
 
     def __init__(self, render_mode = None, size = 3, win_r = 10, loss_r = -10, draw_r = 1) -> None:
-        '''
-        Contructor. Defines the parameters of the board, the state space and action space.
-        State Space: All possible variations of the sizexsize boards. each state is one particular board
-        Action space: choosing to fill a particular cell in the board (for a 3x3 board, at every turn there are 9 options, though not all are valid)
-        Reward: +10 for a win, -10 for a loss, +1 for a draw.
+        """
+            Defines the parameters of the board, the state space and action space.
+            State Space: All possible variations of the sizexsize boards. each state is one particular board
+            Action space: choosing to fill a particular cell in the board (for a 3x3 board, at every turn there are 9 options, though not all are valid)
+            Reward: +10 for a win, -10 for a loss, +1 for a draw for player 1, and a win/loss reward for player 2
 
-        Note: State space must be a list of discrete and valid states of the board, i.e. no cell in a state can hold 2 colors etc..
-        '''
+            Note: State space must be a list of discrete and valid states of the board, i.e. no cell in a state can hold 2 colors etc..
+        """
         self.board_size = (size, size)
         self.num_actions = size**2
-        self.state_space = spaces.Discrete(3**(size**2))
+        state_space = spaces.Discrete(3**(size**2))
+        self.observation_space = state_space*() # calculate the discrete combinations of all valid tic tac toe boards. Need to cacluate for any board size n.
         self.action_space = spaces.Discrete(self.num_actions)
         self.win_reward = win_r
         self.loss_reward = loss_r
@@ -26,27 +27,91 @@ class TicTacToe(gym.Env):
         self.reset()
     
     def reset(self):
-        '''
+        """
             New empty board. info stores the status of the palyers' moves.
-        '''
+        """
         self.state = np.zeros(self.board_size)
         self.info = {"players": {1: {"actions": []}, 2: {"actions": []}}}
         return self.state.flatten(), self.info
 
-    def step():
-        '''
-        Take turn.
-        '''
+    def step_p1(self, action):
+        """
+            Doc String
+        """
+        terminated = truncated = False
+        reward = 0
 
-    def render():
-        '''
-        render the pygame.
-        '''
+        # update self.state = new board setting
+        '''Code'''
+        
+        # check if the game is over
+        if self.gameEndCheck(player = 1):
+            terminated = True
+            reward = self.win_reward
+        else:
+            if len(self.info['players'][1]['actions']) >= 9:
+                truncated = True
+                reward = self.draw_reward
+            else:
+                terminated = True
+                reward = self.loss_reward
+        self.render()
     
-    def buttonClick():
-        '''
-        get input from the user who is one of the player and get s' (new state).
-        '''
+    def step_p2(self, action):
+        """
+        Doc String
+        """
+        terminated = truncated = False
+        reward = 0
+
+        # update self.state = new board setting
+        '''Code'''
+        
+        # check if the game is over
+        if self.gameEndCheck(player = 2):
+            terminated = True
+            reward = self.win_reward
+        else:
+            if len(self.info['players'][1]['actions']) >= 9:
+                truncated = True
+                reward = self.draw_reward
+            else:
+                terminated = True
+                reward = self.loss_reward
+        self.render()
+
+    def render(self, mode="human") -> None:
+        """render the board
+
+        The following charachters are used to represent the fields,
+            '-' no stone
+            'O' for player 1
+            'X' for player 2
+
+        example:
+            ╒═══╤═══╤═══╕
+            │ O │ - │ - │
+            ├───┼───┼───┤
+            │ - │ X │ - │
+            ├───┼───┼───┤
+            │ - │ - │ - │
+            ╘═══╧═══╧═══╛
+        """
+        board = np.zeros((3, 3), dtype=str)
+        for ii in range(3):
+            for jj in range(3):
+                if self.state[ii, jj] == 0:
+                    board[ii, jj] = "-"
+                elif self.state[ii, jj] == 1:
+                    board[ii, jj] = "X"
+                elif self.state[ii, jj] == 2:
+                    board[ii, jj] = "O"
+
+        if mode == "human":
+            board = tabulate(board, tablefmt="fancy_grid")
+
+        # instead of return, use pygame to view it.
+        return board
     
     def gameEndCheck(self, player):
         '''
@@ -70,3 +135,6 @@ class TicTacToe(gym.Env):
                 done = True
                 break
         return done
+    
+if __name__ == "__main__":
+    env = gym.envs.make("TTT-v0")
